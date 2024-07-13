4709cecbc0e99550776fc930ce8f1b734922d192
nrgsnd <nargesnadiasadabad@gmail.com>
2024-07-13 17:25:26 +0330

    Change fillTriangles function

--- DataAcquisition.py
+++ DataAcquisition.py
@@ -4,6 +4,9 @@ import os
 import cv2
 import numpy as np
 import globalVariables
+from Camera import Camera
+from pypylon import pylon
+import scipy
 
 # Data Acquisition Thread
 class DataAcquisitionThread(threading.Thread):
@@ -13,12 +16,14 @@ class DataAcquisitionThread(threading.Thread):
         self.firstPatch = 1
         self.cameraImagePointer = 0
         self.minBackgroundLevel = 235
-        #where super patch started
+        #Where super patch started
         self.startOfBuffer = 0    
         self.localCameraImage = []
         self.localMask = []
         self.localRawImage = []
         self.localImage = []
+        #Grab image from camera or simulator
+        self.grabFromCamera = False
 
         globalVariables.sheetDetect = 0
         globalVariables.cameraDetect = 0
@@ -27,27 +32,68 @@ class DataAcquisitionThread(threading.Thread):
         globalVariables.endOfSheet = 0
         globalVariables.lineReceived = 0
         globalVariables.procBufferEmpty = 1
-        self.i = 0
+        
+        
+        
+        
 
     def run(self):
-        data = self.loadDatasetFolder()
-        #fill top border of first super patch
+        #Camera setting
+        if self.grabFromCamera:
+            camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+            camera.Open()
+            camera.Width.Value = globalVariables.sensorSize
+            camera.Height.Value = globalVariables.linePackSize
+            camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly) 
+        else:
+            #Image from simulator
+            data = self.loadDatasetFolder()
+            index = 0
+        #Fill top border of first super patch
         globalVariables.dataBuffer[:globalVariables.borderSize,:] = globalVariables.backgroundGrayLevel
         self.bufferPointer = globalVariables.borderSize
-        #super patch 0 start from index 0
+        #Super patch 0 start from index 0
         self.startOfBuffer = 0
-        
-        for x in data:
-            #get linepack
-            
-            image = cv2.imread(x,cv2.IMREAD_GRAYSCALE)
-            self.storeCameraImage(image)
-            globalVariables.lineReceived += globalVariables.linePackSize
-                  
-            if self.allLinesAreEmpty(image):
-                if globalVariables.sheetDetect != 1:
-                    continue
+        while True:
+                # Access the image data
+                if self.grabFromCamera:
+                    grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+                    image = grabResult.GetArray()
+                    grabResult.Release()
                 else:
+                    if index >= len(data):
+                         break
+                    image = cv2.imread(data[index],cv2.IMREAD_GRAYSCALE)
+                    index += 1
+      
+                self.storeCameraImage(image)
+                globalVariables.lineReceived += globalVariables.linePackSize
+                    
+                if self.allLinesAreEmpty(image):
+                    if globalVariables.sheetDetect != 1:
+                        continue
+                    else:
+                        if self.bufferPointer - self.startOfBuffer >= globalVariables.superPatchSize:
+                            if globalVariables.procBufferEmpty == 0:
+                                print("non real time")
+                                globalVariables.nonRealtime = 1
+                                continue
+                            else:
+                                self.handleProcessingBuffer(image)
+                        else:
+                            self.SavePackLine(image)
+                            if self.bufferPointer - self.startOfBuffer >= globalVariables.superPatchSize:
+                                if globalVariables.procBufferEmpty == 0:
+                                    print("non real time")
+                                    globalVariables.nonRealtime = 1
+                                    continue
+                                else:
+                                    self.handleProcessingBuffer(image)
+                                
+                            else:
+                                continue
+                else:
+                    globalVariables.sheetDetect = 1
                     if self.bufferPointer - self.startOfBuffer >= globalVariables.superPatchSize:
                         if globalVariables.procBufferEmpty == 0:
                             print("non real time")
@@ -64,36 +110,17 @@ class DataAcquisitionThread(threading.Thread):
                                 continue
                             else:
                                 self.handleProcessingBuffer(image)
-                            
-                        else:
-                            continue
-            else:
-                globalVariables.sheetDetect = 1
-                if self.bufferPointer - self.startOfBuffer >= globalVariables.superPatchSize:
-                    if globalVariables.procBufferEmpty == 0:
-                        print("non real time")
-                        globalVariables.nonRealtime = 1
-                        continue
-                    else:
-                        self.handleProcessingBuffer(image)
-                else:
-                    self.SavePackLine(image)
-                    if self.bufferPointer - self.startOfBuffer >= globalVariables.superPatchSize:
-                        if globalVariables.procBufferEmpty == 0:
-                            print("non real time")
-                            globalVariables.nonRealtime = 1
-                            continue
-                        else:
-                            self.handleProcessingBuffer(image)
+
+            
 
 
     # Functions 
                     
     def SavePackLine(self, image):
-        #store raw image and mask
+        #Store raw image and mask
         self.storeRawImage(image)
         self.createAndStoreMask(image)
-        #save packLine in data buffer
+        #Save packLine in data buffer
         start = self.bufferPointer% globalVariables.bufferSize
         globalVariables.dataBuffer[start:start+globalVariables.linePackSize//2,
                                     globalVariables.borderSize:globalVariables.sensorSize+globalVariables.borderSize] = image[:globalVariables.linePackSize//2, :]
@@ -106,17 +133,22 @@ class DataAcquisitionThread(threading.Thread):
 
 
     def dataBufferToProcessBuffer(self):
-        #process Buffer is empty
-        #compute start and end of super patch in data buffer
+        #Process Buffer is empty
+        #Compute start and end of super patch in data buffer
         s = self.startOfBuffer % globalVariables.bufferSize
         end = globalVariables.superPatchSize - (globalVariables.bufferSize - s)
         if s == 0:
             globalVariables.processBuffer[:, :] = globalVariables.dataBuffer[:globalVariables.superPatchSize, :]
         else:
             globalVariables.processBuffer[:, :] = np.vstack((globalVariables.dataBuffer[s:, :],globalVariables.dataBuffer[:end, :]))
-        #fill triangles resulting from rotation and padding around image with sheet border value
+        #Fill triangles resulting from rotation and padding around image with sheet border value
+        #globalVariables.padding = True
+        s = time.time()
         self.fillTriangles()
+        print("fill triangles time:", time.time() - s)
+        s = time.time()
         self.paddingLeftRightTopButtom()
+        print("padding time:", time.time() - s)
         globalVariables.procBufferEmpty = 0 #set process Buffer is full
         globalVariables.patchCounter += 1
         self.startOfBuffer = self.startOfBuffer + globalVariables.patchSize
@@ -137,7 +169,7 @@ class DataAcquisitionThread(threading.Thread):
 
 
     def endOfSheet(self):
-        #save all image and reset buffers
+        #Save all image and reset buffers
         globalVariables.patchCounter = 0
         globalVariables.rawImage.append(np.vstack(self.localRawImage))
         globalVariables.imageMask.append(np.vstack(self.localMask))
@@ -154,7 +186,7 @@ class DataAcquisitionThread(threading.Thread):
         files = os.listdir(image_dir)
         files.sort(key=lambda x: int(x.split('.')[0]))
         for image_type in files:
-            # load images
+            #Load images
             image_type_dir = os.path.join(image_dir, image_type)
             x.append(image_type_dir)
         return list(x)       
@@ -170,48 +202,18 @@ class DataAcquisitionThread(threading.Thread):
                 return
         max_contour = max(contours, key=cv2.contourArea)
         # Find the bounding rectangle of the sheet
-        y, x, h, w = cv2.boundingRect(max_contour)
-        x_min, x_max, y_min, y_max = x, x+w, y, y+h  
+        x, y, w, h = cv2.boundingRect(max_contour)
+        sub_mask = mask[y:y+h, x:x+w]
+        nearest_neighbor = scipy.ndimage.distance_transform_cdt(sub_mask == 0,metric='taxicab' ,return_distances=False, return_indices=True) 
+        nn_rows, nn_cols = nearest_neighbor
+        sub_img = globalVariables.processBuffer[y:y+h, x:x+w]
+        # Fill triangles with non-zero nearest neighbor 
+        sub_img[sub_mask == 0] = sub_img[nn_rows[sub_mask == 0], nn_cols[sub_mask == 0]]
 
-        # Find non-zero indices in rows and columns
-        nonzero_rows_indices = [np.nonzero(mask[i, :])[0] for i in range(x_min, x_max)]
-        nonzero_cols_indices = [np.nonzero(mask[:, j])[0] for j in range(y_min, y_max)]
+        # Place the processed sub-region back into the original image
+        globalVariables.processBuffer[y:y+h, x:x+w] = sub_img
 
-        for i in range(x_min, x_max):
-            if np.any(mask[i,:] == 0):
-                for j in range(y_min, y_max):
-                    if mask[i, j] == 0:
-                        minsR, maxesR = nonzero_cols_indices[j - y_min][0], nonzero_cols_indices[j - y_min][-1]
-                        minsC, maxesC = nonzero_rows_indices[i - x_min][0], nonzero_rows_indices[i - x_min][-1]
-
-                        
-                        #left border
-                        if i < minsR:
-                            if j < minsC:
-                                if (minsR - i) < (minsC - j):
-                                        globalVariables.processBuffer[i, j] = globalVariables.processBuffer[minsR, j]
-                                else:
-                                        globalVariables.processBuffer[i, j] = globalVariables.processBuffer[i, minsC]
-
-                            if j > maxesC:
-                                if (minsR - i) < (j - maxesC):
-                                        globalVariables.processBuffer[i, j] = globalVariables.processBuffer[minsR, j]
-                                else:
-                                        globalVariables.processBuffer[i, j] = globalVariables.processBuffer[i, maxesC]
-                        #right border
-                        if i > maxesR:
-                            if j < minsC:
-                                if (i - maxesR) < (minsC - j):
-                                        globalVariables.processBuffer[i, j] = globalVariables.processBuffer[maxesR, j]
-                                else:
-                                        globalVariables.processBuffer[i, j] = globalVariables.processBuffer[i, minsC]
-                                            
-                            if j > maxesC:
-                                if (i - maxesR) <= (j - maxesC):
-                                        globalVariables.processBuffer[i, j] = globalVariables.processBuffer[maxesR, j]
-                                else:
-                                        globalVariables.processBuffer[i, j] = globalVariables.processBuffer[i, maxesC]
-                
+          
     
     def paddingLeftRightTopButtom(self):
         R, C = globalVariables.processBuffer.shape
@@ -220,8 +222,8 @@ class DataAcquisitionThread(threading.Thread):
         # Erode the binary image
         mask = cv2.erode(mask, kernel, iterations=1)
 
-        #padding left and right
-        # Find the minimum and maximum non-zero indices along each row
+        #Padding left and right
+        #Find the minimum and maximum non-zero indices along each row
         min_indices = np.argmax(mask, axis=1)
         max_indices = C - np.argmax(np.flip(mask, axis=1), axis=1) - 1
         
@@ -239,22 +241,22 @@ class DataAcquisitionThread(threading.Thread):
                  
                  
     
-        # Update processBuffer for padding left and right
+        #Update processBuffer for padding left and right
         for r in range(R):
             min_val = globalVariables.processBuffer[r, min_indices[r]]
             max_val = globalVariables.processBuffer[r, max_indices[r]]
             globalVariables.processBuffer[r, :min_indices[r]] = min_val
             globalVariables.processBuffer[r, max_indices[r]+1:] = max_val
        
-        #padding top and bottom
+        #Padding top and bottom
         _, mask = cv2.threshold(globalVariables.processBuffer, self.minBackgroundLevel, globalVariables.backgroundGrayLevel, cv2.THRESH_BINARY_INV)
-        # Erode the binary image
+        #Erode the binary image
         mask = cv2.erode(mask, kernel, iterations=1)
         ind = np.where(mask[:, 0] != 0)
         if len(ind[0]) > 0:
             minimum,maximum = min(ind[0]),max(ind[0])
-            globalVariables.processBuffer[:minimum, :] = globalVariables.processBuffer[minimum, :]
-            globalVariables.processBuffer[maximum:, :] = globalVariables.processBuffer[maximum, :] 
+            globalVariables.processBuffer[globalVariables.leftColumn:minimum, :] = globalVariables.processBuffer[minimum, :]
+            globalVariables.processBuffer[maximum: globalVariables.rightColumn, :] = globalVariables.processBuffer[maximum, :] 
             globalVariables.bottomRow = maximum
 
 
